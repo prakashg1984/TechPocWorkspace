0e28e3c28209163117272383f8f1611ca23f6130
prakashg1984 <prakashg84@gmail.com>
2022-01-15 19:21:31 -0500

    datajpa

--- springboot-data-jpa/src/main/java/com/pg/springjpa/DataSourceConfig.java
+++ springboot-data-jpa/src/main/java/com/pg/springjpa/DataSourceConfig.java
@@ -1,11 +1,11 @@
-package com.pg.config;
+package com.pg.springjpa;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
--- springboot-data-jpa/src/main/java/com/pg/springjpa/SpringbootDataJpa2Application.java
+++ springboot-data-jpa/src/main/java/com/pg/springjpa/SpringbootDataJpa2Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class SpringbootDataJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataJpa2Application.class, args);
+	}
+
 }
