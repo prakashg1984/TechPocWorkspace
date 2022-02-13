33d61d2175f1b0c51e18952903509ca2e2e880cf
prakashg1984 <prakashg84@gmail.com>
2022-02-13 17:42:11 -0500

    changes

--- springboot-reactive/src/main/java/com/pg/spring/reactive/controller/UserController.java
+++ springboot-reactive/src/main/java/com/pg/spring/reactive/controller/UserController.java
@@ -1,22 +1,36 @@
 package com.pg.spring.reactive.controller;
 
-import com.pg.spring.reactive.dto.UserDepartmentDTO;
-import com.pg.spring.reactive.model.User;
-import com.pg.spring.reactive.service.UserService;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pg.spring.reactive.dto.UserDepartmentDTO;
+import com.pg.spring.reactive.model.User;
+import com.pg.spring.reactive.service.UserService;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 @Autowired
 private UserService userService;
+
+	static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,7 +40,10 @@ private UserService userService;
 
     @GetMapping
     public Flux<User> getAllUsers(){
-        return userService.getAllUsers();
+    	logger.info("Before GetAllUsers");
+    	Flux<User> userResponse = userService.getAllUsers();
+    	logger.info("After GetAllUsers");
+    	return userResponse;
     }
 
     @GetMapping("/{userId}")
