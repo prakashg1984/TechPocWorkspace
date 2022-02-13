055366d002ae775936a1b032268c619a69f5f7e2
prakashg1984 <prakashg84@gmail.com>
2022-02-13 13:05:34 -0500

    Springboot reactive

---
+++ springboot-reactive/src/main/java/com/pg/spring/reactive/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.pg.spring.reactive.controller;
+
+import com.pg.spring.reactive.dto.UserDepartmentDTO;
+import com.pg.spring.reactive.model.User;
+import com.pg.spring.reactive.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+@Autowired
+private UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<User> create(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public Flux<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public Mono<ResponseEntity<User>> getUserById(@PathVariable Integer userId){
+        Mono<User> user = userService.findById(userId);
+        return user.map( u -> ResponseEntity.ok(u))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{userId}")
+    public Mono<ResponseEntity<User>> updateUserById(@PathVariable Integer userId, @RequestBody User user){
+        return userService.updateUser(userId,user)
+                .map(updatedUser -> ResponseEntity.ok(updatedUser))
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
+    }
+
+    @DeleteMapping("/{userId}")
+    public Mono<ResponseEntity<Void>> deleteUserById(@PathVariable Integer userId){
+        return userService.deleteUser(userId)
+                .map( r -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/age/{age}")
+    public Flux<User> getUsersByAge(@PathVariable int age) {
+        return userService.findUsersByAge(age);
+    }
+
+    @PostMapping("/search/id")
+    public Flux<User> fetchUsersByIds(@RequestBody List<Integer> ids) {
+        return userService.fetchUsers(ids);
+    }
+
+    @GetMapping("/{userId}/department")
+    public Mono<UserDepartmentDTO> fetchUserAndDepartment(@PathVariable Integer userId){
+        return userService.fetchUserAndDepartment(userId);
+    }
+
+}
---
+++ springboot-reactive/src/main/java/com/pg/spring/reactive/model/User.java
@@ -0,0 +1,44 @@
+package com.pg.spring.reactive.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("users")
+public class User {
+
+    @Id
+    private Integer id;
+    private String name;
+    private int age;
+    private double salary;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
